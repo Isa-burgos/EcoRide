1cbaef864506f3bab5a9849efa9a1a4ecfe0809c
Isa-burgos <i.burgos.sabelle@gmail.com>
2025-01-24 09:37:56 +0100

    Add dashboard page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/login", "Se connecter", "/pages/login.html"),
     new Route("/register", "Créer un compte", "/pages/register.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/dashboard", "Mon compte", "/pages/dashboard.html"),
+    new Route("/account", "Mon compte", "/pages/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
