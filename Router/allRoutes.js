3d0d43ae37562ae9b1c1bcd2c4c9142234e2c625
Isa-burgos <i.burgos.sabelle@gmail.com>
2025-01-30 11:39:05 +0100

    Add vue covoiturage page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,13 +3,14 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/login", "Se connecter", "/pages/login.html"),
-    new Route("/register", "Créer un compte", "/pages/register.html"),
+    new Route("/login", "Se connecter", "/pages/auth/login.html"),
+    new Route("/register", "Créer un compte", "/pages/auth/register.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/dashboard", "Mon compte", "/pages/dashboard.html"),
-    new Route("/account", "Mon compte", "/pages/account.html"),
+    new Route("/dashboard", "Mon compte", "/pages/auth/dashboard.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
     new Route("/results", "Covoiturages", "/pages/results-covoiturages.html"),
+    new Route("/trip", "Trajet", "/pages/vue-covoiturage.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
